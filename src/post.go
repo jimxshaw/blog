9208b14411b381ada7efa9f1fbb919243b71c63b
Jim Shaw <jimxshaw@gmail.com>
2022-05-04 22:19:08 -0500

    Add CreateUrl method.

--- src/post.go
+++ src/post.go
@@ -2,6 +2,9 @@ package blog
 
 import (
 	"html/template"
+	"log"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -20,3 +23,19 @@ const (
 	dateParser        = "Date: "
 	tagsParser        = "Tags: "
 )
+
+func CreateUrl(title string) string {
+	// Remove all spaces within the title and replace it with a dash.
+	title = strings.ToLower(strings.Replace(title, " ", "-", -1))
+
+	// Regex will mark anything NOT listed here.
+	regex, err := regexp.Compile("[^a-z0-9\\-]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := regex.ReplaceAllString(title, "")
+	url = strings.Replace(url, "---", "-", -1)
+
+	return "/blog/" + url
+}
